b5da7ac38fc7d3512227a8944e45390a371c2a7d
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2024-03-09 21:27:36 +0900

    update: noindex

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,6 +4,7 @@ export default function App({ Component }: PageProps) {
     <html>
       <head>
         <meta charset="utf-8" />
+        <meta name="robots" content="noindex" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>tranquil</title>
         <link rel="stylesheet" href="/styles.css" />
