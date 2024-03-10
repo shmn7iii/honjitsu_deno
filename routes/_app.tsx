d590a2a775462697e763d71a56847406bbd6f5b2
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2024-03-10 21:26:01 +0900

    update: add ogp

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,12 +5,27 @@ import Header from "../components/Header.tsx";
 export default function App(props: PageProps) {
   return (
     <html>
-      <head>
+      <head prefix="og: https://ogp.me/ns#">
         <meta charset="utf-8" />
         <meta name="robots" content="noindex" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Honjitsu</title>
         <link rel="stylesheet" href="/styles.css" />
+        <meta property="og:url" content={Deno.env.get("DEPLOY_URL")} />
+        <meta property="og:title" content={"Honjitsu"} />
+        <meta property="og:description" content={"Honjitsu"} />
+        <meta property="og:site_name" content={"Honjitsu"} />
+        <meta
+          property="og:image"
+          content={Deno.env.get("DEPLOY_URL") + "/ogp.png"}
+        />
+        <meta name="twitter:title" content={"Honjitsu"} />
+        <meta name="twitter:description" content={"Honjitsu"} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:image"
+          content={Deno.env.get("DEPLOY_URL") + "/ogp.png"}
+        />
       </head>
       <body>
         <div className="flex flex-col min-h-dvh px-12 py-6 lg:px-44">
