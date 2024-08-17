96051c92c85d1b0dca5a13d892982a01ab94b32f
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2024-08-18 00:06:16 +0900

    update: set fallback title

--- components/post/PostTitle.tsx
+++ components/post/PostTitle.tsx
@@ -5,7 +5,7 @@ type Props = {
 export default function PostTitle({ title }: Props) {
   return (
     <h1 className="text-2xl md:text-4xl font-extrabold text-center mb-6">
-      {title}
+      {title || "無題"}
     </h1>
   );
 }
